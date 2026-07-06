d586614b132e8f393be6d0bd7480973485dc836e
1015bean <ibqi1691@gmail.com>
2026-07-06 17:27:08 +0900

    260706 - JPA 전환 작업중 (기본 설정 및 엔티티 작성 완료)

--- src/main/java/com/msa4meerkatgram/domain/post/entities/Post.java
+++ src/main/java/com/msa4meerkatgram/domain/post/entities/Post.java
@@ -1,17 +1,55 @@
 package com.msa4meerkatgram.domain.post.entities;
 
-import lombok.Builder;
+import com.msa4meerkatgram.domain.user.entities.User;
+import jakarta.persistence.*;
 import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.SQLRestriction;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
-// 6db에서 받아온 정보(맵퍼 실행 후)를 담을 엔티티객체
+@Entity
+@EntityListeners(AuditingEntityListener.class)
+@Table(name = "posts")
+@SQLDelete(sql = "UPDATE posts SET deleted_at = NOW() WHERE id = ?")
+@SQLRestriction("deleted_at IS NULL")
 @Getter
-@Builder
+@Setter
 public class Post {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", columnDefinition = "BIGINT UNSIGNED")
     private Long id;
-    private Long userId;
+
+    // user_id: User테이블 엔티티를 가져옴
+    // @현재 엔티티 수 & 연결할 엔티티 수: 테이블 간 관계(다대일, 다대다, 등등)
+    @ManyToOne(fetch = FetchType.LAZY)
+    // 테이블 끼리 join할 때, join해서 가져올 테이블에 대한 설정
+    @JoinColumn(
+            name = "user_id"
+            , insertable = true
+            , updatable = false
+            , nullable = false
+            , foreignKey = @ForeignKey(ConstraintMode.NO_CONSTRAINT))  // 물리적fk 설정하고 싶지 않을 때
+    private User user;
+
+    @Column(name = "content", nullable = false, length = 200)
     private String content;
+
+    @Column(name = "image", nullable = false, length = 100)
     private String image;
+
+    @CreatedDate
+    @Column(name = "created_at", nullable = false)
     private String createdAt;
+
+    @LastModifiedDate
+    @Column(name = "updated_at", nullable = false)
     private String updatedAt;
+
+    @Column(name = "deleted_at", nullable = true)
     private String deletedAt;
 }
--- src/main/java/com/msa4meerkatgram/domain/user/entities/User.java
+++ src/main/java/com/msa4meerkatgram/domain/user/entities/User.java
@@ -1,20 +1,67 @@
 package com.msa4meerkatgram.domain.user.entities;
 
+import com.msa4meerkatgram.global.security.constant.ProviderPolicy;
+import com.msa4meerkatgram.global.security.constant.RolePolicy;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.annotations.JdbcTypeCode;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.SQLRestriction;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
+import java.sql.Types;
+import java.time.LocalDateTime;
+
+@Entity // 해당 클래스가 JPA 엔티티임을 선언
+@EntityListeners(AuditingEntityListener.class) // 엔티티의 이벤트리스너 지정
+@Table(name = "users") // 테이블명 맵핑
+@SQLDelete(sql = "UPDATE users SET deleted_at = NOW() WHERE id = ?") // delete메소드를 커스텀()
+@SQLRestriction("deleted_date IS NULL") // 엔티티의 조회 시 항상 특정 조건을 추가하도록 지정
 @Getter
 @Setter
 public class User {
+    @Id // primary key 지정
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // PK 자동 생성 전략 결정, GenerationType.IDENTITY: 데이터베이스의 AUTO_INCREMENT
+    @Column(name = "id", columnDefinition = "BIGINT UNSIGNED") // 컬럼의 전반적인 속성(컬럼명, 데이터타입, 길이, null여부, 유니크키..) 설정, @Column 없을 경우 데이터타입은 entity 타입 기반으로 자동으로 설정
     private long id;
+
+    @Column(name = "email", unique = true, nullable = false, length = 100)
     private String email;
+
+    @Column(name = "password", nullable = false, length = 255)
     private String password;
+
+    @Column(name = "nick", nullable = false, length = 20)
     private String nick;
-    private String provider;
-    private String role;
+
+    // 들어갈 값으로 이넘 객체를 설정하였음으로, 데이터타입은 이넘 객체로 설정
+    @Column(name = "provider", nullable = false, length = 10)
+    @Enumerated(value = EnumType.STRING) // Enum을 어떤 데이터형식으로 저장할 것인지 설정
+    @JdbcTypeCode(Types.VARCHAR)  // Enum 객체 사용 시, DB의 데이터타입 설정
+    private ProviderPolicy provider = ProviderPolicy.NONE; // 초기값 설정
+
+    @Column(name = "role", nullable = false, length = 10)
+    @Enumerated(value = EnumType.STRING)
+    @JdbcTypeCode(Types.VARCHAR)
+    private RolePolicy role = RolePolicy.NORMAL;
+
+    @Column(name = "profile", nullable = false, length = 100)
     private String profile;
-    private String refreshToken;
-    private String createdAt;
-    private String updatedAt;
-    private String deletedAt;
+
+    @Column(name = "refreshToken", nullable = true, length = 255)
+    private LocalDateTime refreshToken;
+
+    @CreatedDate  // 생성 시 자동으로 시간 입력
+    @Column(name = "created_at", nullable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate // 수정 시 자동으로 시간 업데이트
+    @Column(name = "updated_at", nullable = false)
+    private LocalDateTime updatedAt;
+
+    @Column(name = "deleted_at", nullable = false)
+    private LocalDateTime deletedAt;
 }
